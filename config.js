b30f4d5eac78e893c24fd6b8d36cc3f31a557352
ZixeSea <rene.turok@hotmail.com>
2019-01-30 19:41:53 +0100

    Bettering code grade

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 //730,570,440 (CSGO,DOTA2,TF2)
 module.exports = {
-    username: "Username here",
-    password: "Password here",
-    idle_game_id: [730,570,440]
+    username: "kikkerxd8",
+    password: "Lindeboom1",
+    idleGameId: [730,570,440]
 };
