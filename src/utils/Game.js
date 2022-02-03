89e057f279709079a1a67c14d92f740e68c1b6a2
Rene Lindeboom <rene.turok13@gmail.com>
2022-02-03 23:36:42 +0100

    Added more info to the games

--- src/utils/Game.js
+++ src/utils/Game.js
@@ -5,11 +5,13 @@ class Game {
 		this.playtime = playtime;
 
 		this.idledFor = 0;
+		this.endIdle = 0;
 	}
 
 	async update(time) {
-		this.idledFor = time;
-		this.playtime += time;
+		this.idledFor += time;
+		this.endIdle = Date.now() + time;
+		this.playtime += time / 60000;
 	}
 }
 
