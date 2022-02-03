5200269c1b1cf93ee1b44dc7a64f017f430023a1
Rene Lindeboom <rene.turok13@gmail.com>
2022-02-03 23:36:20 +0100

    Added selectRandomGame and createGameList

--- src/utils/additional.js
+++ src/utils/additional.js
@@ -1,7 +1,21 @@
+const blacklistGames = require('../config/account').idleOptions.blacklist;
+const Game = require('./Game');
+
 module.exports = {
 	getIdleDuration: () => {
 		let random = Math.floor(Math.random() * 20 + 5);
 		return (random *= 120000);
+	},
+	selectRandomGame: (gameList) => {
+		let random = Math.floor(Math.random() * gameList.length);
+		return gameList[random];
+	},
+	createGameList: (games, gameList) => {
+		games.forEach((g) => {
+			if (!blacklistGames.includes(g.appid)) {
+				gameList.push(new Game(g.name, g.appid, g.playtime_forever));
+			}
+		});
 	},
 	timeout: (time) => {
 		return new Promise((resolve) => setTimeout(resolve, time));
