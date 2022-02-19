3a1aa09e8eb6f918e48039b6cb885b4f05eb5a2c
Rene Lindeboom <rene.turok13@gmail.com>
2022-02-19 10:20:02 +0100

    Changed the max parallelGameIdle too 32

--- src/config/account.js
+++ src/config/account.js
@@ -1,4 +1,3 @@
-//730,570,440 (CSGO,DOTA2,TF2)
 module.exports = {
 	accOptions: {
 		username: 'kikkerxd8',
@@ -8,7 +7,7 @@ module.exports = {
 	idleOptions: {
 		randomIdleGames: true, // if true it will get all your games and pick random
 		idleFreeGames: true, // Includes free games that you have played before
-		parallelGameIdle: 0, // This will idle multiple games at ones upto 30 (if 0 or 1 is disabled)
+		parallelGameIdle: 0, // This will idle multiple games at ones upto 32 (if 0 or 1 is disabled)
 		staticIdleTime: 0, // if 0 it will created a random number (in MS)
 		blacklist: [], // backlist games from getting randomly idled
 		SkipBannedGames: false, // If true this won't idle games your banned it
